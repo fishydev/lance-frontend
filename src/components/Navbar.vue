b3686827a1260b6c58ef09fd9481a8d52d449b91
fishydev <huvuhuvuhuvu@gmail.com>
2020-04-21 07:29:00 +0700

    added navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-toolbar
+        color="grey"
+        flat>
+        <v-toolbar-title>LANCE</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn text large>Category</v-btn>
+        <v-btn text large>Messages</v-btn>
+        <v-btn text large>Favorites</v-btn>
+        <v-btn text large>Orders</v-btn>
+        <v-avatar>
+            <v-icon large>mdi-account-circle</v-icon>
+        </v-avatar>
+    </v-toolbar>
+</template>
