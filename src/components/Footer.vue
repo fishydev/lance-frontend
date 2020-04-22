f795343753ad31c177edf653aac247bc3d36224f
fishydev <huvuhuvuhuvu@gmail.com>
2020-04-22 07:19:05 +0700

    add miwon

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,80 @@
 <template>
-    <v-footer>
-        
-    </v-footer>
+    <div>
+        <v-footer
+            dark
+            padless
+            color="blue-grey darken-2">
+            <v-col
+                class="text-left"
+                cols="2">
+                <p class="title">LANCE</p>
+            </v-col>
+            <v-col
+                cols="5"
+            >
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="font-weight-black">
+                            Kategori
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                            Musik
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Programming
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Translating
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Writing
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Desain
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-col>
+            <v-col
+                cols="5"
+            >
+                <v-list-item>
+                    <v-list-item-content>
+                        <v-list-item-title class="font-weight-black">
+                            About
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                            Privacy Policy
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Terms of Services
+                        </v-list-item-subtitle>
+                        <v-list-item-title class="font-weight-black">
+                            Support
+                        </v-list-item-title>
+                        <v-list-item-subtitle>
+                            Help and Support
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            Trust and Safety
+                        </v-list-item-subtitle>
+                        <v-list-item-subtitle>
+                            FAQ
+                        </v-list-item-subtitle>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-col>
+        </v-footer>
+    </div>
 </template>
+
+<!-- // <script>
+// export default{
+//     name: 'Footer',
+//     data () {
+//         return {
+//
+//         }
+//     }
+// }
+// </script> -->
