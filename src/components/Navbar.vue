f33eddd148a695ce8fc99e2967190d198edf6d89
fishydev <huvuhuvuhuvu@gmail.com>
2020-04-22 07:19:20 +0700

    navbar component

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,25 @@
 <template>
-    <v-toolbar
-        color="grey"
-        flat>
-        <v-toolbar-title>LANCE</v-toolbar-title>
-        <v-text-field
-            class="ml-4 mt-4"
-            append-outer-icon="mdi-magnify"
+    <div>
+        <v-toolbar
+            color="blue-grey darken-2"
             flat
-            dense></v-text-field>
-        <v-spacer></v-spacer>
-        <v-btn text large>Category</v-btn>
-        <v-btn text large>Messages</v-btn>
-        <v-btn text large>Favorites</v-btn>
-        <v-btn text large>Orders</v-btn>
-        <v-btn
-            tile
-            icon>
-            <v-icon x-large>mdi-account-circle</v-icon>
-        </v-btn>
-    </v-toolbar>
+            dense>
+            <v-toolbar-title>LANCE</v-toolbar-title>
+            <v-text-field
+                class="ml-4 mt-4"
+                append-outer-icon="mdi-magnify"
+                flat
+                dense></v-text-field>
+            <v-spacer></v-spacer>
+            <v-btn text large>Category</v-btn>
+            <v-btn text large>Messages</v-btn>
+            <v-btn text large>Favorites</v-btn>
+            <v-btn text large>Orders</v-btn>
+            <v-btn
+                tile
+                icon>
+                <v-icon x-large>mdi-account-circle</v-icon>
+            </v-btn>
+        </v-toolbar>
+    </div>
 </template>
